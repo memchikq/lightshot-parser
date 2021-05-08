e7783d9c9978f240af02b87593c9604d281d1724
Derks-cz <derks_derks@mail.ru>
2021-05-08 17:20:21 +0600

    new

--- README.md
+++ README.md
@@ -8,10 +8,13 @@ You can use this script to parse screenshots from the site https://prnt.sc
 ### Using
 ```
 const imgParser = require("lightshot-parser")
-// this function has three arguments
+// this function has four arguments
 // the first argument is how many screenshots need to be parsed
 // the second argument is the path to save screensots
-// (optional argument) the third argument is pattern, the link will start with these letters (default - null)
-imgParser(5,"./img","aq1")
+// the third argument is the delay (milliseconds) in sending the request (default - 1000ms)
+// If the requests are too frequent, there is a chance of your IP being banned on the site prnt.sc 
+// I recommend making a delay of a few seconds, the probability of your IP being banned will be lower
+// (optional argument) the fourth argument is pattern, the link will start with these letters (default - null)
+imgParser(15,"./img",2000,"aq1")
 // after calling the function, screenshots will be uploaded to the directory
 ```
--- index.js
+++ index.js
@@ -6,48 +6,52 @@ const path = require("path")
 /**
  *@param {Number} [quantity] Number of screenshots
  *@param {String} [src] Where to download directory
+ *@param {Number} [delay] Delay in sending the request
  *@param {String} [pattern] String pattern
-**/
-function imgParser(quantity,src,pattern = null) {
+ **/
+function imgParser(quantity, src, delay = 1000, pattern = null) {
   let c = quantity
   async function getImg() {
     if (c > 0) {
       try {
         const randomString = "abcdefghijklmnopqrstuvwxyz1234567890"
-        let url = ''
-        if(pattern){
+        let url = ""
+        if (pattern) {
           url = pattern
-          for(let i=0; i < 6 - pattern.length; i++){
-              url += randomString[Math.floor(Math.random()*randomString.length)]
+          for (let i = 0; i < 6 - pattern.length; i++) {
+            url += randomString[Math.floor(Math.random() * randomString.length)]
+          }
+        } else {
+          for (let i = 0; i < 6; i++) {
+            url += randomString[Math.floor(Math.random() * randomString.length)]
           }
         }
-        else {
-          for(let i=0; i < 6; i++){
-            url += randomString[Math.floor(Math.random()*randomString.length)]
-        }
-        }
+
         const response = await fetch(`https://prnt.sc/${url}`)
-        const bufferResponse = await response.arrayBuffer() 
+        const bufferResponse = await response.arrayBuffer()
         const bf = buffer.Buffer.from(bufferResponse)
         const decoder = new TextDecoder("utf-8")
         const decode = decoder.decode(bf)
         const $ = cheerio.load(decode)
         const rows = $(".screenshot-image")
-        if(rows[0].attribs.src && !rows[0].attribs.src.startsWith("/") && !rows[0].attribs.src.match(/i.imgur/g)){
-            c--
-            let img = await fetch(rows[0].attribs.src)
-            const response = await img.arrayBuffer()
-            const imgBuffer = buffer.Buffer.from(response)
-            fs.writeFileSync(path.join(src,`${getCache()}.png`), imgBuffer, 'binary');
+        if (rows[0].attribs.src &&!rows[0].attribs.src.startsWith("/") && !rows[0].attribs.src.match(/i.imgur/g)){
+          c--
+          let img = await fetch(rows[0].attribs.src)
+          const response = await img.arrayBuffer()
+          const imgBuffer = buffer.Buffer.from(response)
+          fs.writeFileSync(path.join(src, `${getCache()}.png`),imgBuffer,"binary")
         }
       } catch (e) {
-       
+        
       }
-      getImg()
+
+      setTimeout(() => {
+        getImg()
+      }, delay)
     }
   }
   getImg()
-} 
+}
 function getCache() {
   const c = "abcdefghijklmnopqrstuvwxyz1234567890"
   let randomWords = ""
